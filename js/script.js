c0e19f2c456abd38afbb5806d87e26cfd3d01719
Piedra17 <163149335+Piedra17@users.noreply.github.com>
2025-06-25 12:57:55 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -26,6 +26,9 @@ leftBtn.addEventListener('click', () => {
 
 window.addEventListener('resize', updateCarousel);
 
+// Llamada inicial
+updateCarousel();
+
 
 const toggleBtn = document.getElementById('toggle-btn');
 const navMenu = document.getElementById('nav-menu');
@@ -33,3 +36,30 @@ const navMenu = document.getElementById('nav-menu');
 toggleBtn.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
+
+
+
+ // acordeon
+  const titulos = document.querySelectorAll(".acordeon-titulo");
+
+  titulos.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const contenido = btn.nextElementSibling;
+
+      btn.classList.toggle("activo");
+
+      // Cierra otros
+      document.querySelectorAll(".acordeon-contenido").forEach((el) => {
+        if (el !== contenido) {
+          el.style.maxHeight = null;
+          el.previousElementSibling.classList.remove("activo");
+        }
+      });
+
+      if (contenido.style.maxHeight) {
+        contenido.style.maxHeight = null;
+      } else {
+        contenido.style.maxHeight = contenido.scrollHeight + "px";
+      }
+    });
+  });
